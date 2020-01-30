78636bb1e30a9b220466e9e7253f0cf1a110ee9b
EnariKun <manao1995@yahoo.co.jp>
2020-01-31 03:05:17 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/manao/kawamori/qa_app/QuestionDetailActivity.kt
@@ -0,0 +1,145 @@
+package jp.techacademy.manao.kawamori.qa_app
+
+import android.content.Intent
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.ListView
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_question_detail.*
+import kotlinx.android.synthetic.main.list_question_detail.*
+import kotlinx.android.synthetic.main.list_question_detail.view.*
+
+import java.util.HashMap
+
+class QuestionDetailActivity : AppCompatActivity() {
+
+    private lateinit var mQuestion: Question
+    private lateinit var mAdapter: QuestionDetailListAdapter
+    private lateinit var mAnswerRef: DatabaseReference
+    private var user: FirebaseUser? = null
+    private lateinit var favoriteState: DatabaseReference
+
+    private val mEventListener = object : ChildEventListener {
+        override fun onChildAdded(dataSnapshot: DataSnapshot, s: String?) {
+            val map = dataSnapshot.value as Map<String, String>
+
+            val answerUid = dataSnapshot.key ?: ""
+
+            for (answer in mQuestion.answers) {
+                // 同じAnswerUidのものが存在しているときは何もしない
+                if (answerUid == answer.answerUid) {
+                    return
+                }
+            }
+
+            val body = map["body"] ?: ""
+            val name = map["name"] ?: ""
+            val uid = map["uid"] ?: ""
+
+            val answer = Answer(body, name, uid, answerUid)
+            mQuestion.answers.add(answer)
+            mAdapter.notifyDataSetChanged()
+        }
+
+        override fun onChildChanged(dataSnapshot: DataSnapshot, s: String?) {
+
+        }
+
+        override fun onChildRemoved(dataSnapshot: DataSnapshot) {
+
+        }
+
+        override fun onChildMoved(dataSnapshot: DataSnapshot, s: String?) {
+
+        }
+
+        override fun onCancelled(databaseError: DatabaseError) {
+
+        }
+    }
+
+    private val mInitFavorite = object : ValueEventListener {
+        override fun onDataChange(dataSnapshot: DataSnapshot) {
+            if(dataSnapshot.value != null){
+                favorite.setImageResource(R.drawable.star_enable)
+            } else {
+                favorite.setImageResource(R.drawable.star_disenable)
+            }
+            favorite.show()
+        }
+
+        override fun onCancelled(databaseError: DatabaseError) {
+
+        }
+    }
+
+    private val mChangeFavorite = object : ValueEventListener {
+        override fun onDataChange(dataSnapshot: DataSnapshot) {
+            favorite.hide()
+            if(dataSnapshot.value != null){
+                favoriteState.removeValue()
+                favorite.setImageResource(R.drawable.star_disenable)
+            } else {
+                favoriteState.setValue(mQuestion.genre)
+                favorite.setImageResource(R.drawable.star_enable)
+            }
+            favorite.show()
+        }
+
+        override fun onCancelled(databaseError: DatabaseError) {
+
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_detail)
+        // 渡ってきたQuestionのオブジェクトを保持する
+        val extras = intent.extras
+        mQuestion = extras.get("question") as Question
+
+        title = mQuestion.title
+
+        // ListViewの準備
+        mAdapter = QuestionDetailListAdapter(this, mQuestion)
+        listView.adapter = mAdapter
+        mAdapter.notifyDataSetChanged()
+
+        user = FirebaseAuth.getInstance().currentUser
+        favoriteState  = FirebaseDatabase.getInstance().reference.child(FavoritesPATH).child(user!!.uid).child(mQuestion.questionUid)
+        if(user != null){
+            favoriteState.addListenerForSingleValueEvent(mInitFavorite)
+        }
+
+        fab.setOnClickListener {
+            // ログイン済みのユーザーを取得する
+            if (user == null) {
+                // ログインしていなければログイン画面に遷移させる
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(intent)
+            } else {
+                // Questionを渡して回答作成画面を起動する
+                val intent = Intent(applicationContext, AnswerSendActivity::class.java)
+                intent.putExtra("question", mQuestion)
+                startActivity(intent)
+            }
+        }
+
+        favorite.setOnClickListener{
+            favoriteState.addListenerForSingleValueEvent(mChangeFavorite)
+        }
+
+        val dataBaseReference = FirebaseDatabase.getInstance().reference
+        mAnswerRef = dataBaseReference.child(ContentsPATH).child(mQuestion.genre.toString()).child(mQuestion.questionUid).child(AnswersPATH)
+        mAnswerRef.addChildEventListener(mEventListener)
+    }
+
+}
